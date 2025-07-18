612033f9e99e4ffe26dc07ae4db49465e5650467
medfahmy <medfahmy@fastmail.com>
2025-07-18 01:37:55 +0100

    vertices in storage buffer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::sync::Arc;
+use std::{f32::consts::PI, sync::Arc};
 
 use wgpu::*;
 use winit::{
     application::ApplicationHandler,
     event::WindowEvent,
-    event_loop::{ControlFlow, EventLoop, ActiveEventLoop},
+    event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
     window::{Window, WindowId},
 };
-
 
 fn main() {
     env_logger::init();
@@ -18,7 +17,6 @@ fn main() {
     let mut app = App::default();
     event_loop.run_app(&mut app).unwrap();
 }
-
 
 #[derive(Default)]
 struct App {
@@ -70,7 +68,12 @@ struct RenderContext {
     surface: Surface<'static>,
     format: TextureFormat,
     render_pipeline: RenderPipeline,
-    objects: Vec<Object>,
+    num_instances: u32,
+    base_storage_buffer: Buffer,
+    extra_storage_buffer: Buffer,
+    extra_storage_values: Vec<Extra>,
+    scaling_units: Vec<f32>,
+    bind_group: BindGroup,
     window: Arc<Window>,
 }
 
@@ -130,53 +133,67 @@ impl RenderContext {
             cache: None,
         });
 
-        let num_objects = 100;
-        let mut objects = Vec::with_capacity(num_objects);
+        let num_instances = 100;
 
-        let base_uniform_buffer_size = std::mem::size_of::<Base>().max(32);
-        let extra_uniform_buffer_size = std::mem::size_of::<Extra>().max(32);
+        let base_unit_size = std::mem::size_of::<Base>();
+        let base_storage_buffer_size = base_unit_size * num_instances as usize;
+        let base_storage_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some(&format!("base uniform buffer for obj")),
+            size: base_storage_buffer_size as BufferAddress,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
 
-        for i in 0..num_objects {
-            let base_uniform_buffer = device.create_buffer(&BufferDescriptor {
-                label: Some(&format!("base uniform buffer for obj {}", i)),
-                size: base_uniform_buffer_size as BufferAddress,
-                usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
-                mapped_at_creation: false,
-            });
+        let extra_unit_size = std::mem::size_of::<Extra>();
+        let extra_storage_buffer_size = extra_unit_size * num_instances as usize;
+        let extra_storage_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some(&format!("extra uniform buffer for obj")),
+            size: extra_storage_buffer_size as BufferAddress,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+
+        let mut bases = Vec::with_capacity(num_instances as usize);
+        let mut scaling_units = Vec::with_capacity(num_instances as usize);
+        for i in 0..num_instances as usize {
             let base = Base {
                 color: [rand(0.0, 1.0), rand(0.0, 1.0), rand(0.0, 1.0), 1.0],
                 offset: [rand(-0.9, 0.9), rand(-0.9, 0.9)],
             };
-            queue.write_buffer(&base_uniform_buffer, 0, bytemuck::cast_slice(&[base]));
-
-            let extra_uniform_buffer = device.create_buffer(&BufferDescriptor {
-                label: Some(&format!("extra uniform buffer for obj {}", i)),
-                size: extra_uniform_buffer_size as BufferAddress,
-                usage: BufferUsages::UNIFORM | BufferUsages::COPY_DST,
-                mapped_at_creation: false,
-            });
-
-            let bind_group = device.create_bind_group(&BindGroupDescriptor {
-                label: Some(&format!("bind group for obj {}", i)),
-                layout: &render_pipeline.get_bind_group_layout(0),
-                entries: &[
-                    BindGroupEntry {
-                        binding: 0,
-                        resource: base_uniform_buffer.as_entire_binding(),
-                    },
-                    BindGroupEntry {
-                        binding: 1,
-                        resource: extra_uniform_buffer.as_entire_binding(),
-                    },
-                ],
-            });
-
-            objects.push(Object {
-                scale: rand(0.2, 0.5),
-                extra_uniform_buffer,
-                bind_group,
-            });
+            bases.push(base);
+            scaling_units.push(rand(0.2, 0.5));
         }
+        queue.write_buffer(&base_storage_buffer, 0, bytemuck::cast_slice(&bases));
+
+        let extra_storage_values = Vec::with_capacity(num_instances as usize);
+
+        let vertices = create_circle_vertices(0.5, 24, 0.25, 0.0, PI * 2.0);
+        let vertex_storage_buffer = device.create_buffer(&BufferDescriptor {
+            label: Some("vertex storage buffer"),
+            size: (std::mem::size_of::<Vertex>() * vertices.len()) as BufferAddress,
+            usage: BufferUsages::STORAGE | BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        queue.write_buffer(&vertex_storage_buffer, 0, bytemuck::cast_slice(&vertices));
+
+        let bind_group = device.create_bind_group(&BindGroupDescriptor {
+            label: Some(&format!("bind group")),
+            layout: &render_pipeline.get_bind_group_layout(0),
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: base_storage_buffer.as_entire_binding(),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: extra_storage_buffer.as_entire_binding(),
+                },
+                BindGroupEntry {
+                    binding: 2,
+                    resource: vertex_storage_buffer.as_entire_binding(),
+                },
+            ],
+        });
 
         Self {
             device,
@@ -184,7 +201,12 @@ impl RenderContext {
             surface,
             format,
             render_pipeline,
-            objects,
+            num_instances,
+            base_storage_buffer,
+            extra_storage_buffer,
+            extra_storage_values,
+            scaling_units,
+            bind_group,
             window,
         }
     }
@@ -210,7 +232,6 @@ impl RenderContext {
             format: Some(self.format.add_srgb_suffix()),
             ..Default::default()
         });
-
 
         let mut encoder = self
             .device
@@ -238,19 +259,19 @@ impl RenderContext {
         });
         render_pass.set_pipeline(&self.render_pipeline);
 
-
         let size = self.window.inner_size();
         let aspect = size.width / size.height;
-
-        for object in &mut self.objects {
-            let uniform = Extra {
-                scale: [object.scale / aspect as f32, object.scale],
-            };
-            self.queue
-                .write_buffer(&object.extra_uniform_buffer, 0, bytemuck::cast_slice(&[uniform]));
-            render_pass.set_bind_group(0, &object.bind_group, &[]);
-            render_pass.draw(0..3, 0..1);
-        }
+        let extras: Vec<_> = self
+            .scaling_units
+            .iter()
+            .map(|scale| Extra {
+                scale: [scale / aspect as f32, *scale],
+            })
+            .collect();
+        self.queue
+            .write_buffer(&self.extra_storage_buffer, 0, bytemuck::cast_slice(&extras));
+        render_pass.set_bind_group(0, &self.bind_group, &[]);
+        render_pass.draw(0..24 * 3 * 2, 0..self.num_instances);
 
         drop(render_pass);
 
@@ -275,12 +296,48 @@ struct Extra {
     scale: [f32; 2],
 }
 
-struct Object {
-    scale: f32,
-    extra_uniform_buffer: Buffer,
-    bind_group: BindGroup,
-}
-
 fn rand(min: f32, max: f32) -> f32 {
     min + rand::random_range(0.0..1.0) * (max - min)
+}
+
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, Pod, Zeroable)]
+struct Vertex {
+    position: [f32; 2],
+}
+
+impl Vertex {
+    fn new(x: f32, y: f32) -> Self {
+        Self { position: [x, y] }
+    }
+}
+
+fn create_circle_vertices(
+    radius: f32,
+    num_subdivisions: usize,
+    inner_radius: f32,
+    start_angle: f32,
+    end_angle: f32,
+) -> Vec<Vertex> {
+    let mut vertices = Vec::new();
+
+    for i in 0..num_subdivisions {
+        let angle1 = start_angle + i as f32 * (end_angle - start_angle) / num_subdivisions as f32;
+        let angle2 =
+            start_angle + (i as f32 + 1.0) * (end_angle - start_angle) / num_subdivisions as f32;
+
+        let c1 = angle1.cos();
+        let s1 = angle1.sin();
+        let c2 = angle2.cos();
+        let s2 = angle2.sin();
+
+        vertices.push(Vertex::new(c1 * radius, s1 * radius));
+        vertices.push(Vertex::new(c2 * radius, s2 * radius));
+        vertices.push(Vertex::new(c1 * inner_radius, s1 * inner_radius));
+        vertices.push(Vertex::new(c1 * inner_radius, s1 * inner_radius));
+        vertices.push(Vertex::new(c2 * radius, s2 * radius));
+        vertices.push(Vertex::new(c2 * inner_radius, s2 * inner_radius));
+    }
+
+    vertices
 }
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,3 +1,12 @@
+struct Vertex {
+    position: vec2f,
+};
+
+struct VSOutput {
+    @builtin(position) position: vec4f,
+    @location(0) color: vec4f,
+};
+
 struct Base {
     color: vec4f,
     offset: vec2f,
@@ -7,21 +16,26 @@ struct Extra {
     scale: vec2f,
 };
 
-@group(0) @binding(0) var<uniform> base: Base;
-@group(0) @binding(1) var<uniform> extra: Extra;
+@group(0) @binding(0) var<storage, read> bases: array<Base>;
+@group(0) @binding(1) var<storage, read> extras: array<Extra>;
+@group(0) @binding(2) var<storage, read> vertices: array<Vertex>;
 
 @vertex
-fn vs_main(@builtin(vertex_index) vertex_index: u32) -> @builtin(position) vec4f {
-    let positions = array(
-        vec2f(0.0, 0.5),
-        vec2f(-0.5, -0.5),
-        vec2f(0.5, -0.5),
+fn vs_main(
+    @builtin(vertex_index) vertex_index: u32,
+    @builtin(instance_index) instance_index: u32,
+) -> VSOutput {
+    var out: VSOutput;
+    out.position = vec4f(
+        vertices[vertex_index].position * extras[instance_index].scale + bases[instance_index].offset, 
+        0.0,
+        1.0,
     );
-
-    return vec4f(positions[vertex_index] * extra.scale + base.offset, 0.0, 1.0);
+    out.color = bases[instance_index].color;
+    return out;
 }
 
 @fragment
-fn fs_main() -> @location(0) vec4f {
-    return base.color;
+fn fs_main(out: VSOutput) -> @location(0) vec4f {
+    return out.color;
 }
